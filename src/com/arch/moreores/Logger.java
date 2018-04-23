0d9a9009773858965f70fb3fb8b3783360610dc7
BuildTools <unconfigured@null.spigotmc.org>
2018-04-23 19:30:36 +1000

    Added ability to reload plugin with /moreores reload. Added ability to enable or disable form ingame with /moreores [enable/disable].

--- src/com/arch/moreores/Logger.java
+++ src/com/arch/moreores/Logger.java
@@ -12,7 +12,7 @@ public class Logger {
 
     /*logger for MoreOres*/
     public void log(String message) {
-        String logMessage = ChatColor.GOLD + "[MoreOres] " + ChatColor.RESET + message;//adds plugin name to front of log
+        String logMessage = plugin.config.prefix + message;//adds plugin name to front of log
         /*Sends message to log */
         plugin.getServer().getConsoleSender().sendMessage(logMessage);
 
